cf2f5cccc2e231b97746b76f3b60478f32fb3d95
Ali Albarrak <alaa13212@gmail.com>
2020-07-23 23:04:03 +0300

    Implementing MvvmCross Tutorial

---
+++ README.md
@@ -0,0 +1,2 @@
+# TipCalc
+This project is just for learning and experimenting proposes. It is basically implementation of [MvvmCross](https://www.mvvmcross.com/documentation/tutorials/tipcalc/the-tip-calc-tutorial) tutorial.
---
+++ TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,48 @@
+﻿using MvvmCross.ViewModels;
+using System.Threading.Tasks;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+	public class TipViewModel : MvxViewModel
+	{
+		private readonly ICalculationService _calculationService;
+
+		public TipViewModel(ICalculationService calculationService)
+		{
+			_calculationService = calculationService;
+		}
+
+		public override async Task Initialize()
+		{
+			await base.Initialize();
+
+
+		}
+
+		private double _subTotal;
+		public double SubTotal {
+			get => _subTotal;
+			set { SetProperty(ref _subTotal, value); Recalculate(); }
+		}
+
+		private int _generosity;
+		public int Generosity {
+			get => _generosity;
+			set { SetProperty(ref _generosity, value); Recalculate(); }
+		}
+
+
+		private double _tip;
+		public double Tip {
+			get => _tip;
+			set { SetProperty(ref _tip, value); }
+		}
+
+
+		private void Recalculate()
+		{
+			Tip = _calculationService.TipAmount(SubTotal, Generosity);
+		}
+	}
+}
